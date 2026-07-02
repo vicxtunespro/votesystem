201a7ec5198791285be89b6324ffc11b73746692
Nuwarimpa Victor <victordementa@gmail.com>
2026-07-02 14:25:59 +0000

    vps setup

--- includes/conn.php
+++ includes/conn.php
@@ -1,8 +1,12 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'votesystem');
 
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
-	
-?>
+$host = getenv('MYSQL_HOST') ?: 'mysql';
+$db   = getenv('MYSQL_DATABASE');
+$user = getenv('MYSQL_USER');
+$pass = getenv('MYSQL_PASSWORD');
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
