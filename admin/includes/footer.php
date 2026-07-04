1f8f8482133dc5a0d841e54e19c4a835353374dc
Nuwarimpa Victor <victordementa@gmail.com>
2026-07-04 03:22:10 +0000

    fix: Footer

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -2,5 +2,5 @@
     <div class="pull-right hidden-xs">
       <b>All rights reserved</b>
     </div>
-    <strong>Copyright &copy; 2024 <a href="https://www.projectarena.vercel.app">NSANGI SECONDARY SCHOOOL</a></strong>
+    <strong>Copyright &copy; 2026 <a href="https://www.projectarena.vercel.app">NSANGI SECONDARY SCHOOOL</a></strong>
 </footer>
--- db/voters.sql
+++ db/voters.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 06, 2024 at 05:05 PM
+-- Generation Time: Apr 06, 2026 at 05:05 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
